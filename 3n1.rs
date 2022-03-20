fbce5a6e721147bda65bd48bfa3ce44c23cb0124
vada4367 <88112844+vada4367@users.noreply.github.com>
2022-03-20 11:55:21 +0300

    3n+1 task
    )))

---
+++ 3n1.rs
@@ -0,0 +1,22 @@
+ 
+ fn main(){
+    let a :u128 = 97896903033925991790370000000000000;
+    let mut b :u128 = 9789690303392599179035;
+    while b <= a{
+        let mut c :u128 = b;
+        let mut check :bool = false;
+        while !check{
+            if c % 2 == 0{
+                c = c / 2;
+            }
+            else{
+                c = c * 3 + 1;
+            }
+            if c == 4 || c == 2 || c == 4{
+                println!("number {} check", b);
+                check = true;
+            }
+        }
+        b = b + 1;
+    }
+}
